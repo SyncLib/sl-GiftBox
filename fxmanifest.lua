29b6617206e56fa52178ac0ae2eaa1c89dc38b96
ImMacky <mackydevelopment0@gmail.com>
2023-03-13 17:17:51 -0700

    Initial files

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+description 'Free, open source starter gift box'
+author 'Macky'
+version '1.0'
+
+server_script 'server/*.lua'
+
+client_script 'client/*.lua'
+
+shared_script 'config.lua'
