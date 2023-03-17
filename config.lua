3027a92653d482978cc4ef43db47c325eb1feff5
Macky <104384493+ImMacky@users.noreply.github.com>
2023-03-16 18:47:30 -0700

    Update config.lua

---
+++ config.lua
@@ -0,0 +1,16 @@
+Config = {}
+
+-- Rewards Table (add rewards below) --
+Config.Rewards = {
+    {Name = "tosti", Amount = 10},
+    {Name = "water_bottle", Amount = 10},
+    {Name = "bandage", Amount = 8},
+    {Name = "lockpick", Amount = 5},
+    {Name = "radio", Amount = 1},
+    {Name = "lighter", Amount = 1},
+}
+
+-- Gift Box Setup / Config --
+Config.MaxRewards = 3 -- Max Rewards the play will receive on-opening GiftBox
+
+Config.ProgressBarInteger = 25000 -- Amount of time for all progress bars to complete (ms)
